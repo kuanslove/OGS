abfccbbe90fd74dbab3126d79806c1ffa97a7a66
kuanslove <kuanslove@hotmail.com>
2013-07-26 16:20:38 -0400

    add user admin function, add page divider

--- app.js
+++ app.js
@@ -26,26 +26,25 @@ function set_env(){
 	app.use(express.logger('dev'));
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
-
 	app.use(express.cookieParser('you can never crack this secret key, can you?'));
 	app.use(express.session());
-	
+
 	app.use(express.static(path.join(__dirname, 'public')));
-	app.use(function(req, res, next){
-		var dt=new Date();
-		if((dt.getHours()<19)&&(dt.getHours()>6)){
-			// if it is shop hour: 6AM~12AM, we allow user to visit shop.
-			next();
-		}
-		else {
-			if(req.url!="/nevertrytodothis/thisisadmin/"){
-				res.render("btn");
-			}
-			else {
-				next();
-			}
-		}
-	});
+	//~ app.use(function(req, res, next){
+		//~ var dt=new Date();
+		//~ if((dt.getHours()<24)&&(dt.getHours()>6)){
+			//~ // if it is shop hour: 6AM~12AM, we allow user to visit shop.
+			//~ next();
+		//~ }
+		//~ else {
+			//~ if(req.url!="/never/"){
+				//~ res.render("btn");
+			//~ }
+			//~ else {
+				//~ next();
+			//~ }
+		//~ }
+	//~ });
 	app.use(app.router);
 
 	// development only
@@ -67,7 +66,9 @@ function check_https(req, res){
 		return false;
 	}
 };
-// First, we parse r to figure out SQL query string for finding product.
+
+
+
 function build_category_sql(r){
 	
 };
@@ -100,7 +101,7 @@ function build_user_sql(r){
 	
 	
 	//~ var sql="SELECT FROM products WHERE n_a=false ";
-	var sql="WITH p AS ( SELECT * FROM users ";
+	var sql="WITH p AS ( SELECT * FROM users  ";
 	var holder_index=1;
 	var params = [];
 	
@@ -111,14 +112,22 @@ function build_user_sql(r){
 			placeholder.push('$'+(holder_index));
 			params.push('%'+r.keywd[holder_index-1]+'%');
 		}
-		sql+="WHERE email ILIKE ANY (array["+placeholder.join(",")+"]) ";
+		sql+="WHERE (email ILIKE ANY (array["+placeholder.join(",")+"]) ";
+		
+		var placeholder = [];
+		for(holder_index=1; holder_index<=r.keywd.length; holder_index++){
+			placeholder.push('$'+(holder_index+r.keywd.length));
+			params.push('%'+r.keywd[holder_index-1]+'%');
+		}
+		sql+="OR name ILIKE ANY (array["+placeholder.join(",")+"])) ";
+		holder_index = 2*r.keywd.length+1;
 	}
 	
 	sql+=") ";
 	sql+="SELECT *, (SELECT count(*) FROM p) as total FROM p ";
 	
 	
-	sql+="ORDER BY date_rev DESC ";
+	//sql+="ORDER BY date_rev DESC ";
 	
 	if(r.result_num){
 		params.push(r.result_num);
@@ -132,6 +141,11 @@ function build_user_sql(r){
 	
 	return {sql:sql, params:params};
 };
+
+
+
+
+
 
 function build_product_sql(r){
 	
@@ -208,7 +222,6 @@ function build_product_sql(r){
 	
 	return {sql:sql, params:params};
 };
-
 function build_my_product_sql(r){
 	
 	// the r passed in should be like
@@ -278,6 +291,33 @@ function is_authed(req, res){
 		return false;
 	}
 };
+
+function is_admin_authed(req, res){
+	console.log("the auth admin user_id is:", req.cookies.user_id);
+	if(req.session.user_id && req.cookies.user_id && req.session.admn){
+		if(req.cookies.user_id==req.session.user_id){
+			// user_id matched, authed!
+			return true;
+		}
+		else {
+			// fake user_id, denied!
+			return false;
+		}
+	}
+	else {
+		// no user_id, denied!
+		return false;
+	}
+};
+
+
+// This get_product function(down below) can be use query any thing rather than product.
+// So, we use it globally.
+// the return JSON loks like:
+//						{
+//							total_num:r_num,
+//							page_list:result.rows
+//						}
 function get_product(req, res, r){
 	// we move this function outside, so we can use this for both common
 	// product request and my product request
@@ -296,6 +336,7 @@ function get_product(req, res, r){
 				done();
 				if(err){
 					// err could be "table not exists"!
+					console.log(err);
 					res.send(404, 'Sorry, query is sleeping!');
 				}
 				else {
@@ -306,6 +347,7 @@ function get_product(req, res, r){
 					else {
 						r_num=result.rows[0].total;
 					}
+					console.log(result.rows);
 					res.json(
 						{
 							total_num:r_num,
@@ -373,27 +415,22 @@ app.get("/test/", function(req, res){
 
 
 
-
-app.get('/user/', function(req,res){
-	pg.connect(constring, function(err, client, done){
-		if(err){
-			done();
-			return console.error('Error to connect. ', err);
+app.post('/s_user/admin/', function(req,res){
+	if(check_https(req,res)) {
+		var r = clone(req.body);
+		var qq = build_user_sql(r);
+		
+		if( is_authed(req, res) ){
+			get_product(req, res, qq);
 		}
 		else {
-			var qstring="select * from users";
-			client.query(qstring, function(err, result){
-				done();
-				if(err){
-					return console.error('Error to fetch data. ', err);
-				}
-				else {
-					res.json(result);
-				}
-			});
+			res.send(404, "User not authed to get product list.")
 		}
-	});
+	}
+
 });
+
+
 app.get('/', function(req, res){
 	if(check_https(req,res)){
 		
@@ -477,14 +514,37 @@ app.post('/auth/', function(req, res){
 		}
 	}
 });
+
+app.post('/auth/admin/', function(req, res){
+	if( check_https(req, res) ){
+		// is_authed check if req.session.user_id exists 
+		// and match the one passed from client
+		if( is_admin_authed(req, res) ){
+			res.json( {
+				authed:true,
+				user_id:req.session.user_id,
+				user_email:req.session.user_email,
+				message:"You are Authed!"
+				});
+		}
+		else {
+			res.json( {
+				authed:false,
+				message:"You should Login!"
+				});
+		}
+	}
+});
+
 app.post('/login/', function(req, res){
 	if( check_https(req, res) ){
 		if( is_authed(req, res) ){
-			// if authed, then logout the user		
-			console.log("the user_id is in session.")
+			// if authed, then logout the user
+			// this case is for logout button click
 			req.session.user_id=undefined;
 			res.clearCookie('user_id');
-			res.json({'login':false, 'user_id':undefined});
+			//~ res.json({'login':false, 'user_id':undefined});
+			res.render("index");
 		}
 		else {
 			// if not authed, then we verify and login the user
@@ -510,12 +570,78 @@ app.post('/login/', function(req, res){
 								res.cookie('user_id',result.rows[0].id, {maxAge:2000000});
 								req.session.user_id=result.rows[0].id;
 								req.session.user_email=result.rows[0].email;
+								if(result.rows[0].admn){
+									console.log("this is a administrator.")
+									req.session.admn=true;
+								}
+								else {
+									req.session.admn=false;
+								}
 								res.json({'login':true, 'user_email':result.rows[0].email});
 							}
 							else {
 					// user email passwd not matched.
 								req.session.user_id=undefined;
 								req.session.user_email=undefined;
+								req.session.admn=false;
+								res.clearCookie('user_id');
+								res.json({'login':false, 'user_id':undefined});
+							}
+						}
+					});
+				}
+			});			
+			
+			
+			
+			
+		}
+		
+	
+	}
+	
+});
+app.post('/login/admin/', function(req, res){
+	if( check_https(req, res) ){
+		if( is_authed(req, res) ){
+			// if authed, then logout the user
+			req.session.user_id=undefined;
+			res.clearCookie('user_id');
+			//~ res.json({'login':false, 'user_id':undefined});
+			res.render("index");
+		}
+		else {
+			// if not authed, then we verify and login the user
+			pg.connect(constring, function(err, client, done){
+				if(err){
+					// the server refuse to connect.
+					done();
+					res.send(501, 'Sorry, server is busy playing XBOX right now!');
+				}
+				else {
+					// the server accepts connect
+					var qstring="select * from users where n_a=false and admn=true and email=$1 and pswd=md5($2)";
+					client.query(qstring, [req.body.email,req.body.email+req.body.passwd],function(err, result){
+						done();
+					// the server is good, but can not query.
+						if(err){
+							res.send(404, 'Sorry, query is sleeping!');
+						}
+						else {
+					// successfully query
+							console.log(result.rows);
+							if(result.rows.length!=0){
+								res.cookie('user_id',result.rows[0].id, {maxAge:2000000});
+								req.session.user_id=result.rows[0].id;
+								req.session.user_email=result.rows[0].email;
+								req.session.admn=true;
+								res.json({'login':true, 'user_email':result.rows[0].email});
+							}
+							else {
+					// user email passwd not matched.
+								req.session.user_id=undefined;
+								req.session.user_email=undefined;
+								req.session.admn=false;
 								res.clearCookie('user_id');
 								res.json({'login':false, 'user_id':undefined});
 							}
@@ -664,30 +790,99 @@ app.post("/u_pswd/",function(req, res){
 		}
 	}	
 });
-app.get("/nevertrytodothis/thisisadmin/", function(req, res){
+
+app.post("/a_user/", function(req,res){
 	if(check_https(req,res)) {
-		var date = new Date();
-		var t = date.getHours();
-		console.log(date.getHours().toString());
-		res.send(200,date.getHours().toString());
+		if(is_authed(req, res)){
+			pg.connect(constring, function(err, client, done){
+				if(err){
+					done();
+					res.send(501, 'Sorry, server is busy playing XBOX right now!');
+				}
+				else {
+					var qstring = "SELECT * from users WHERE email=$1";
+					client.query(qstring, [req.body.email],function(err, result){
+						if(err){
+							console.log(err);
+							res.send(404, 'Sorry, err happed during find dup email!');
+						}
+						else {
+							console.log(result);
+							if(result.rowCount==0){
+								var qstring="INSERT INTO users (name, email, admn,n_a, pswd) VALUES ($1,$2, $3,$4,md5($5))";
+								var b=req.body;
+								var placeholder = [b.name, b.email,b.admn,b.n_a,b.email+'ogs'];
+								client.query(qstring, placeholder,function(err, result){
+									if(err){
+										res.send(404, 'Sorry, adding is sleeping!');
+									}
+									else {
+										res.json({suc:true});
+									}
+								});
+							}
+							else {
+								res.send(404, 'Sorry, this email has already been registered!');
+							}
+							
+						}
+					});
+				}
+			});
+		}
+		else {
+			res.send(404, 'User not authed to add.');
+		}
+	}
+});
+app.post("/u_user/", function(req, res){
+	if(check_https(req,res)) {
+		if(is_authed(req, res)){
+			pg.connect(constring, function(err, client, done){
+				if(err){
+					done();
+					res.send(501, 'Sorry, server is busy playing XBOX right now!');
+				}
+				else {
+					var qstring="UPDATE users SET (admn,n_a)=($1,$2) WHERE id=$3";
+					console.log(req.body.admn,req.body.n_a,req.body.user_id);
+					client.query(qstring, [req.body.admn,req.body.n_a,req.body.user_id],function(err, result){
+						if(err){
+							console.log(err);
+							res.send(404, 'Sorry, updating is sleeping!');
+						}
+						else {
+							console.log(result);
+							res.json({suc:true});
+						}
+					});
+				}
+			});
+		}
+		else {
+			res.send(404, 'User not authed to update.');
+		}
 	}
 });
 
-//~ app.post("",function(req, res){
-	//~ 
-//~ });
 
-//~ app.get('/images/:fn',function(req, res){
-	//~ console.log(req.query.fn);
-	//~ res.sendFile("./public/images/"+req.query.fn);
-//~ });
+app.get("/never/", function(req, res){
+	if(check_https(req,res)) {
+		req.session.user_id=undefined;
+		req.session.admn=false;
+		res.clearCookie('user_id');
+		//~ res.json({'login':false, 'user_id':undefined});
+		res.render('admin');
+	}
+});
+
 
 //~ //The 404 Route (ALWAYS Keep this as the last route)
 app.get('*', function(req, res){
-  res.redirect("/");
+  res.render("btn");
 });
 app.post('*', function(req, res){
-  res.redirect("/");
+  res.render("btn");
 });
 
 // this part just for running server
